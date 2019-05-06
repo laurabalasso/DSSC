c6f3e9b51db27e3a3a83e7c34d07f828b9e2ea01
laurabalasso <laurabalasso96@gmail.com>
2019-05-06 16:41:42 +0200

    file .sh fixed

--- Assignments/Ex_01/ex1.sh
+++ Assignments/Ex_01/ex1.sh
@@ -1,19 +1,13 @@
-cd $HOME/parallel_programming/lesson2
+/bin/hostname
+
+cd $HOME/parallel_programming/lesson2/
+
+export OMP_NUM_THREADS=5
 
 module load intel
 
-icc -qopenmp ex1.c -o ex1.x
-icc -qopenmp ex1_critical.c -o ex1_crit.x
-icc -qopenmp ex1_reduction.c -o ex1_red.x
+icc -qopenmp ex2.c -o ex2
 
-for proc in 1 2 4 8 16 20;
-do
-    export OMP_NUM_THREADS=${proc}; ./ex1.x >> ex1_atomic.out;
-    ./ex1_crit.x >> ex1_crit.out; ex1_red.x >> ex1_red.out;
+./ex2 >> ex2.out
 
-done
-
-
-
-
-
+exit
