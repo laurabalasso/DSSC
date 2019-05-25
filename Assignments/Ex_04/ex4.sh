09874e81cd730f35c5dcddc953492538c34907a2
laurabalasso <laurabalasso96@gmail.com>
2019-05-25 11:41:57 +0200

    ex4.sh updated

--- Assignments/Ex_04/ex4.sh
+++ Assignments/Ex_04/ex4.sh
@@ -1,3 +1,4 @@
+/bin/hostname
 
 cd $HOME/parallel_programming/Assignments/Ex_04
 
@@ -5,11 +6,7 @@ module load openmpi
 
 mpicc ex4.c -o ex4.x
 
-for np in 2 4 8 16 20
-do
-echo "Number of processes: " ${np} >> ex4.out
-(mpirun -np ${np} ./ex4.x) >> ex4.out
-done
+mpirun -np 4 ./ex4.x >> ex4.out
 
 exit
 
